87498b8aeeb693ce17dfec90aa1b814aae4b5655
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-08-23 17:20:20 -0400

    initial changes

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.135.0/http/server.ts";
+
+serve((req) => {
+    try {
+        return new Response(JSON.stringify({
+            epochTime: Date.now(),
+        }), {
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    } catch (err) {
+        return new Response(err);
+    }
+}, { port: 8000 });
